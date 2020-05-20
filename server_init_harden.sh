a1a399b06e408b3e14cbd3d4b97f25fdc42f3dcf
Hoang Tran <trbhoang@gmail.com>
2020-05-20 23:05:33 +0700

    install docker-compose

--- server_init_harden.sh
+++ server_init_harden.sh
@@ -187,3 +187,8 @@ sudo apt update
 sudo apt install docker-ce
 sudo usermod -aG docker $USER
 docker run hello-world
+
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
